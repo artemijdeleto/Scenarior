38b81683990eb2d83e5c40f784c948448c4e64a9
Artemij <artemijdeleto@gmail.com>
2019-10-07 09:51:09 +0400

    Create getter for user authorization state

--- src/store.js
+++ src/store.js
@@ -5,12 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
 	state: {
-
+		user: {
+			id: 0
+		}
 	},
 	mutations: {
 
 	},
 	actions: {
 
+	},
+	getters: {
+		isLogged(state) {
+			if (state.user.id > 0) return true;
+			return false;
+		}
 	}
 })
