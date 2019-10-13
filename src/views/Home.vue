74c81fa2cea93b4ba5d5c41eb98e655aa65e0426
Artemij <artemijdeleto@gmail.com>
2019-10-13 18:36:04 +0400

    Add simple promotion for example only

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,15 +14,39 @@
 
 		<div class="container">
 
-			<section>
-				<h2>Killer Feature #1</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non eligendi maiores mollitia adipisci recusandae ratione, cupiditate aliquam distinctio consequatur id dolorum voluptatibus odio harum animi minus nulla nostrum dolore doloremque.</p>
-			</section>
-			<section>
-				<h2>Even more killer feature</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non eligendi maiores mollitia adipisci recusandae ratione, cupiditate aliquam distinctio consequatur id dolorum voluptatibus odio harum animi minus nulla nostrum dolore doloremque.</p>
-			</section>
+			<div class="row mb-4">
+				<div class="d-none d-sm-block col-4">
+					<img class="shadow rounded w-100" src="https://images.unsplash.com/photo-1515734392280-e60c25eb9f01?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80" alt="">
+				</div>
+				<div class="col">
+					<h2 class="mt-0">Design your characters</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non eligendi maiores mollitia adipisci recusandae ratione, cupiditate aliquam distinctio consequatur id dolorum voluptatibus odio harum animi minus nulla nostrum dolore doloremque.</p>
+				</div>
+			</div>
+			<div class="row mb-4">
+				<div class="col">
+					<h2 class="mt-0">Create your own look</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non eligendi maiores mollitia adipisci recusandae ratione, cupiditate aliquam distinctio consequatur id dolorum voluptatibus odio harum animi minus nulla nostrum dolore doloremque.</p>
+				</div>
+				<div class="d-none d-sm-block col-4">
+					<img class="shadow rounded w-100" src="https://images.unsplash.com/photo-1468857006728-bba1dba0eb7f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1032&q=80" alt="">
+				</div>
+			</div>
+			<div class="row mb-4">
+				<div class="d-none d-sm-block col-4">
+					<img class="shadow rounded w-100" src="https://images.unsplash.com/photo-1515634928627-2a4e0dae3ddf?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="">
+				</div>
+				<div class="col">
+					<h2 class="mt-0">Turn the idea into complete screenplay</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non eligendi maiores mollitia adipisci recusandae ratione, cupiditate aliquam distinctio consequatur id dolorum voluptatibus odio harum animi minus nulla nostrum dolore doloremque.</p>
+				</div>
+			</div>
 
+			<p class="text-center text-muted">
+				<strong>Scenarior</strong> — your friend in making graceful screenplays
+				<br>
+				<small>Using <strong>Scenarior</strong> is completely free</small>
+			</p>
 		</div>
 	</div>
 </template>
